1576b0b930561ba122b3d912a9b2a3305e344f50
Gaurav Sharma <gsharma@readywireless.com>
2014-10-01 15:53:26 -0500

    initial commit

---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yoAngularApp.controller:ContactCtrl
+ * @description
+ * # ContactCtrl
+ * Controller of the yoAngularApp
+ */
+angular.module('yoAngularApp')
+  .controller('ContactCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
