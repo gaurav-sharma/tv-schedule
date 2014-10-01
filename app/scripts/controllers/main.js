f86232e61a9edae01ee4f7ff93f3a5cc1fe7f955
Gaurav Sharma <gsharma@readywireless.com>
2014-10-01 16:49:50 -0500

    basic list

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,15 +3,43 @@
 /**
  * @ngdoc function
  * @name yoAngularApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the yoAngularApp
+ * @description # MainCtrl Controller of the yoAngularApp
  */
-angular.module('yoAngularApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('yoAngularApp').controller(
+		'MainCtrl',
+		function($scope, $http, API_KEY) {
+			$scope.apiKey = API_KEY;
+			$scope.filterText = null;
+			$scope.results = [];
+
+			$scope.init = function() {
+				// API requires a start date
+				var today = new Date();
+				// Create the date string and ensure leading zeros if required
+				var apiDate = today.getFullYear()
+						+ ("0" + (today.getMonth() + 1)).slice(-2) + ""
+						+ ("0" + today.getDate()).slice(-2);
+				$http.jsonp(
+						'http://api.trakt.tv/calendar/premieres.json/'
+								+ $scope.apiKey + '/' + apiDate + '/' + 30
+								+ '/?callback=JSON_CALLBACK').success(
+						function(data) {
+
+							angular.forEach(data, function(value, index) {
+
+								var date = value.date;
+
+								angular.forEach(value.episodes, function(
+										tvshow, index) {
+
+									tvshow.date = date;
+
+									$scope.results.push(tvshow);
+								});
+							});
+						}).error(function(error) {
+
+				});
+			};
+
+		});
---
+++ app/scripts/services/apikey.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name yoAngularApp.apiKey
+ * @description
+ * # apiKey
+ * Constant in the yoAngularApp.
+ */
+angular.module('yoAngularApp')
+  .constant('API_KEY', '637a468fe83e483e79eaeefa93849329');
